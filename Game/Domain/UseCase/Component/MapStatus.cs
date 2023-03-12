9c833ff2ababd6cd3a1f217773ccfdcab3741b16
EF <eorf256@gmail.com>
2023-03-12 16:37:28 +0900

    リファクタ

--- Game/Domain/UseCase/Component/MapStatus.cs
+++ Game/Domain/UseCase/Component/MapStatus.cs
@@ -4,19 +4,18 @@ public class MapStatus
 {
     public int Length => _status.Length;
     
-    private GameDefine.State[] _status;
-
-    private int _length;
+    private readonly GameDefine.State[] _status;
+    private readonly int _length;
     
     public GameDefine.State this[int i]
     {
-        get { return _status[i]; }
-        set { _status[i] = value; }
+        get => _status[i];
+        set => _status[i] = value;
     }
     public GameDefine.State this[Vector2Int v]
     {
-        get { return _status[v.x + v.y * _length]; }
-        set { _status[v.x + v.y * _length] = value; }
+        get => _status[v.x + v.y * _length];
+        set => _status[v.x + v.y * _length] = value;
     }
 
     public MapStatus(GameDefine.State[] status, int length)
